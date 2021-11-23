b0c7cd16e622e3b5353c2e04947b0b94f78fc127
Opilane TTHK <opilane@tthk.ee>
2021-11-23 16:29:27 +0200

    week13

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Ylesanne3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ToyotaData = @"C:\Users\opilane\samples\Week13\toyota.txt";
+            string BMWData = @"C:\Users\opilane\samples\Week13\bmw.txt";
+            string CarsData = @"C:\Users\opilane\samples\Week13\cars.txt";
+
+            List<string> toyota = new List<string>();
+            List<string> bmw = new List<string>();
+
+            string[] sourceData = File.ReadAllLines(CarsData);
+
+            foreach (string element in sourceData)
+            {
+                if (element.Contains("Toyota"))
+                {
+                    toyota.Add(element);
+
+                }
+                if (element.Contains("BMW"))
+                {
+                    bmw.Add(element);
+
+                }
+                File.WriteAllLines(ToyotaData, toyota);
+                File.WriteAllLines(BMWData, bmw);
+
+            }
+
+
+
+
+        }
+
+
+
+    }
+}
